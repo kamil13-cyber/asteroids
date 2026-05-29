e1592fc092153878cbc653e582d69572909c4df4
kamil13-cyber <chojnygrind13@gmail.com>
2026-05-29 13:25:27 +0200

    1c: More changes to the structure

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from  logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    print("Starting Asteroids with pygame version: " + pygame.version.ver)
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0.0
 
     while True:
         log_state()
@@ -19,11 +15,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-    screen.fill("black")
+        screen.fill("black")
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
+
+        print(dt)
 
 if __name__ == "__main__":
     main()
